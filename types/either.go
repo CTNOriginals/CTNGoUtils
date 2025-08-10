5c3533d3e3d093d172e17a206d8f50ae26ae2dce
CTN <info@ctn-originals.com>
2025-08-10 21:09:12 +0200

    Create either.go
    A union typ that can hold either type A or B.
    This was initially created to be able to help the ToString method of map, but i gave up and split it to up anyway. Might still be useful for something else tho.

---
+++ types/either.go
@@ -0,0 +1,23 @@
+package ctype
+
+type Either[A, B any] struct {
+	value any
+}
+
+func (e *Either[A, B]) SetA(a A) {
+	e.value = a
+}
+
+func (e *Either[A, B]) SetB(b B) {
+	e.value = b
+}
+
+func Switch[A, B, R any](e *Either[A, B], onA func(a A) R, onB func(b B) R) R {
+	switch v := e.value.(type) {
+	case A:
+		return onA(v)
+	case B:
+		return onB(v)
+	}
+	return *new(R)
+}
